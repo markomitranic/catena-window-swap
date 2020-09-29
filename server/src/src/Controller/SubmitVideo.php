4da1258607751d61b38c40f55e257116af909b95
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-29 10:19:40 +0200

    Breaking off from googles shitty api

--- server/src/src/Controller/SubmitVideo.php
+++ server/src/src/Controller/SubmitVideo.php
@@ -3,15 +3,24 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\YouTube\VideoUploadService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class SubmitVideo extends AbstractController
 {
 
-	public function index()
+	public function index(Request $request, VideoUploadService $uploadService): JsonResponse
 	{
-		return new Response('test');
+		$filesBag = $request->files->all();
+		if (!array_key_exists('video', $filesBag)) {
+			throw new \Exception('No video attached', 400);
+		}
+
+		$uploadService->upload($filesBag['video'], $request->get('name'), $request->get('location'));
+
+		return new JsonResponse(true);
 	}
 
 }
