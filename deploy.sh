a1e24799ef751366067b664ce789d9b487355b44
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-29 15:15:41 +0200

    Update ignored folders to contain temporary ones

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ docker build ./node -t windowswap_node --target prod
 
 docker-compose down --remove-orphans
 docker-compose build
-docker-compose up --remove-orphans
+docker-compose up --remove-orphans -d
