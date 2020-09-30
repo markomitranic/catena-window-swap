bf2d05cd528313f631785d3e2a3c043ad00512cb
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-30 20:32:57 +0200

    Additional FE updates

--- node/src/scripts/Video/VideoPanel.js
+++ node/src/scripts/Video/VideoPanel.js
@@ -10,16 +10,22 @@ class VideoPanel {
 		this.videoProvider = videoProvider;
 		this.embedCodeResolver = embedCodeResolver;
 		this.iframeWrapper = iframeWrapper;
+		this.nameLabel = document.querySelector('.video-info .uploader');
+		this.locationLabel = document.querySelector('.video-info .location span');
 	}
 
 	next() {
 		this.videoProvider.get((video) => {
 			this.iframeWrapper.innerHTML = this.embedCodeResolver.getEmbedCode(video.getVideoId());
+			this.nameLabel.innerHTML = `${video.name}'s View`
+			this.locationLabel.innerHTML = video.location
 		});
 	}
 
 	off() {
 		this.iframeWrapper.innerHTML = '';
+		this.nameLabel.innerHTML = '';
+		this.locationLabel.innerHTML = '';
 	}
 
 }
